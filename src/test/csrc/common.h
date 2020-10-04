7db48b09195521d9843080bd60e9287570c87e71
Ivyfeather <chenxi171@mails.ucas.ac.cn>
2020-10-04 16:20:37 +0800

    starting test framework

---
+++ src/test/csrc/common.h
@@ -0,0 +1,9 @@
+#ifndef __COMMON_H
+#define __COMMON_H
+
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <stdint.h>
+#include <assert.h>
+
---
+++ src/test/csrc/difftest.h
@@ -0,0 +1,6 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <stdint.h>
+#include <assert.h>
+#include <string.h>
